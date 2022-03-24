a4fc3f6f8ddd60c3c89bd13104478ba17168a8de
Hincu Andrei <45599037+Saxdel@users.noreply.github.com>
2022-03-24 12:08:49 +0200

    Test

---
+++ Slot_Test/Controllers/SlotGame.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Slot_Test.Controllers
+{
+    public class SlotGame : ControllerBase
+    {
+        public SlotGame()
+        {
+            
+        }
+    }
+}
