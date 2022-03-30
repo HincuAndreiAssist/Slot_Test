712d4a6d0289d5e48d3ab9db4c875b4ac7d5633d
Radu Flocea <radu.flocea@assist.ro>
2022-03-30 15:08:57 +0300

    Modified the reels

--- Slot_Test/Entities/Strips.cs
+++ Slot_Test/Entities/Strips.cs
@@ -7,8 +7,8 @@ namespace Slot_Test.Entities
 {
     public class Strips
     {
-        public string ReelOne { get; set; }
-        public string ReelTwo { get; set; }
-        public string ReelThree { get; set; }
+        public int ReelOne { get; set; }
+        public int ReelTwo { get; set; }
+        public int ReelThree { get; set; }
     }
 }
